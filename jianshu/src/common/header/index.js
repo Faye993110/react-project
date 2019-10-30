cfc87cb58e5ef0f3e5eb337e3dcc7a63576a9e9c
Faye <faye@Fayes-MacBook-Pro-2.local>
2019-10-31 01:08:10 +0800

    header icon step1

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import '../../statics/confont/iconfont.css'
+
 function Header() {
   return (
     <div className='headerWrapper'>
         <a className='logo' href='/'></a>
         <div className='nav'>
-            <div className='navItem left'>首页</div>
+            <div className='navItem left active'>首页</div>
             <div className='navItem left'>下载APP</div>
             <div className='navItem right'>登陆</div>
-            <div className='navItem right'>Aa</div>
+            <div className='navItem right'><i className='iconfont'></i></div>
+            <input className = 'navSearch' placeholder="搜索"></input>
+        </div>
+        <div className='addition'>
+            <div className = "button writting">写文章</div>
+            <div className = "button reg">注册</div>
         </div>
     </div>
   );
