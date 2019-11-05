a0e9547d3902cd2f5f48e59a97129f10da79818a
Faye993110 <470917066@qq.com>
2019-11-06 00:33:53 +0800

    add redux-devtool extension and add combineReducers

--- jianshu/src/store/index.js
+++ jianshu/src/store/index.js
@@ -3,10 +3,8 @@ import reducer from './reducer'
 import { createStore, applyMiddleware, compose } from 'redux';
 
 //const store = createStore(reducer);
-
-const store = createStore(reducer, /* preloadedState, */ compose(
-    applyMiddleware(...middleware)
-  ));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers());
 
 
 export default store;
--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,20 +1,10 @@
-const defaultState = {
-    focused: false
-};
+import {combineReducers} from 'redux';
+import {reducer as headerReducer} from '../common/header/store';
+
+const reducer = combineReducers({
+    header: headerReducer
+})
+
+export default reducer;
 
 
-//纯函数，给定固定的输入，一定有固定的输出，并且不会改变原来的值
-
-export default (state = defaultState, action) => {
-    if(action.type === 'search_focus'){
-        return{
-            focused:true
-        }
-    }
-    if(action.type === 'search_blur'){
-        return{
-            focused:false
-        }
-    }
-  return state
-}
