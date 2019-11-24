00d03a60c1fafe83ad680333f28f1ba294b326f2
Faye993110 <470917066@qq.com>
2019-11-24 23:48:34 +0800

    add react router

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -3,12 +3,21 @@ import {Globalstyle} from './style.js';
 import Header from './common/header/index';
 import store from './store';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
+      <div>
       <Header></Header>
+      <BrowserRouter>
+        <div>
+        <Route path = '/' exact render = { () => <div>home</div>}></Route>
+        <Route path = '/detail' exact render = { () => <div>detail</div>}></Route>
+        </div>
+      </BrowserRouter>
       <Globalstyle/>
+      </div>
     </Provider>
   );
 }
