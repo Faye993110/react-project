f3e02cd234abeea5e30bb4d56812be97284c0d1e
Faye993110 <470917066@qq.com>
2019-11-26 01:01:15 +0800

    finish home layout

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import './style.css'
+import Topic from './components/Topic';
+import List from './components/List';
+import Writer from './components/Writer';
+import Recommend from './components/Recommend';
+
 
 class Home extends  Component {
     render(){
         return(
-            <div>
-                Home
+            <div className = 'homeWrapper'>
+                <div className = 'homeLeft'>
+                    <img className = 'banner-img' src="//upload-images.jianshu.io/upload_images/12063224-0b83bafc00f4e2f0.jpeg?imageMogr2/auto-orient/strip|imageView2/2/w/1200/format/webp"></img>
+                    <Topic></Topic>
+                    <List></List>
+                </div>
+                <div className = 'homeRight'>
+                    <Recommend></Recommend>
+                    <Writer></Writer>
+                </div>
             </div>
         )
     }
